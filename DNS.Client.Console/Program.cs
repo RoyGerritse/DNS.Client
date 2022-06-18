5d07f0b69b5765de4f261caef8d3f7e8ed77ecb6
Roy Gerritse <info@roygerritse.nl>
2022-06-18 17:17:41 +0200

    initial setup

---
+++ DNS.Client.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DNS.Client;
+
+namespace DNS.Client.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            var dnsClient = new DnsClient();
+            dnsClient.Request();
+        }
+    }
+}
---
+++ DNS.Client/DnsClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DNS.Client
+{
+    public class DnsClient
+    {
+        public DnsClient()
+        {
+            
+        }
+
+        public void Request()
+        {
+
+
+
+            string text = "roygerritse.nl: type A, class IN";
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+
+
+            var client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("8.8.8.8"), 53); // endpoint where server is listening
+            client.Connect(ep);
+
+            // send data
+            client.Send(bytes);
+
+            // then receive data
+            var receivedData = client.Receive(ref ep);
+            var str = Encoding.ASCII.GetString(receivedData);
+            Console.Write("receive data from " + ep.ToString());
+             Console.WriteLine(str);
+            Console.Read();
+        }
+    }
+}
